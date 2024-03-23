4fde05ec98b5b30f8c41a37b9c0ac69c9f2db659
poonam7809 <poonamjoshicodes@gmail.com>
2024-03-23 17:15:29 +0530

    msg

--- index.js
+++ index.js
@@ -2,4 +2,14 @@
 // git config --global user.email "email"
 // git config --list
 
+// git init
+//git status (untracked, modified, unmodified, commited)
+// git add [file name]
+// git add .
+// git commit -m ""
+// git remote add origin <-url->
+// git branch (to check branch)
+// git branch -m [new name]
+// git push origin main
+// git push -v origin main (git push)
 console.log('hello');
